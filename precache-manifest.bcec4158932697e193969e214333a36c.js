e00d12b6eed09aee37103e5b2a2ebd2ce932b271
AKolte <adi.kolte@gmail.com>
2020-06-16 13:35:35 +0530

    Updates

---
+++ precache-manifest.bcec4158932697e193969e214333a36c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb23f66192d196d71ffa8638cd96921",
+    "url": "/ReduxTodo/index.html"
+  },
+  {
+    "revision": "a21a3b4aed06891557c8",
+    "url": "/ReduxTodo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90242f62c43c7e4b2eac",
+    "url": "/ReduxTodo/static/js/2.b96a96b5.chunk.js"
+  },
+  {
+    "revision": "28f49bec5ac2dc8ce9a86de5d1b1939e",
+    "url": "/ReduxTodo/static/js/2.b96a96b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21a3b4aed06891557c8",
+    "url": "/ReduxTodo/static/js/main.a597a92d.chunk.js"
+  },
+  {
+    "revision": "c04557294280eedeb2e7",
+    "url": "/ReduxTodo/static/js/runtime-main.906587dc.js"
+  }
+]);
